1011abfb6d8102684a85df983a7d31530d71daf3
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2023-07-27 13:47:18 +0200

    Clean project

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,20 @@
 //npm install @types/dat.gui --save-dev
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import * as THREE from 'three'
+
 const scene = new THREE.Scene()
-
-const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
-
-camera1.position.z = 2
-
-const canvas1 = document.getElementById('canvas1') as HTMLCanvasElement
-let renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 })
-let HD = false;
-let pixels = HD ? 2000 : 500;
-renderer1.setSize(pixels, pixels)
-
-const orbitControls = new OrbitControls(camera1, renderer1.domElement);
-scene.add(orbitControls)
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+let rendrerer = new THREE.WebGLRenderer({ canvas: canvas })
+function setup(){
+    camera.position.z = 2
+    let HD = false;
+    let pixels = HD ? 2000 : 500;
+    rendrerer.setSize(pixels, pixels)
+    const orbitControls = new OrbitControls(camera, rendrerer.domElement);
+    scene.add(orbitControls)
+}
+setup()
 
 
 const groundGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -29,8 +29,6 @@ scene.add(groundMesh);
 
 function update() {
     requestAnimationFrame(update)
-
-    renderer1.render(scene, camera1)
+    rendrerer.render(scene, camera)
 }
-
 update()
