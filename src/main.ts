44ec3b2432f5618e9e59ffbc269d6192c41ebbe5
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2024-09-17 13:18:01 +0200

    Refactor start project

--- src/main.ts
+++ src/main.ts
@@ -5,37 +5,26 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xf8f5f2);
 
 const camera = new THREE.PerspectiveCamera(75, 1920 / 1080, 0.1, 10);
-camera.position.y = 1.5;
-camera.position.z = 2;
+camera.position.set(0, 1.5, -2);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.rotateY(Math.PI / 4);
+scene.add(boxMesh);
+
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0.5, 1.5, -2);
+light.target = boxMesh;
+scene.add(light);
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 let renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(1920, 1080);
-
-const groundGeometry = new THREE.BoxGeometry(1, 1, 1);
-const groundMaterial = new THREE.MeshPhongMaterial();
-const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-groundMesh.position.y = 0;
-groundMesh.rotateY(Math.PI / 4);
-scene.add(groundMesh);
-
-const light = new THREE.AmbientLight(0xff0000); // red light
-light.intensity = 1;
-scene.add(light);
-
-const light2 = new THREE.DirectionalLight(0xffffff); // white light
-light2.intensity = 0.5;
-light2.position.set(1, 1, 2);
-light2.target = groundMesh;
-light2.castShadow = true;
-scene.add(light2);
-
-camera.lookAt(new THREE.Vector3(0, 0, 0));
 function update() {
-  requestAnimationFrame(update);
-
   //groundMesh.rotateY(Math.PI / 60);
-
+  requestAnimationFrame(update);
   renderer.render(scene, camera);
 }
 update();
